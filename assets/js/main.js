f4e4a4b5611315f8dfccd3cda359602301bee423
rasul sönmez <barbaros743@outlook.com>
2022-01-01 18:58:07 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+let navbar = document.querySelector(".header .navbar");
+
+document.querySelector("#menu-button").onclick = () => {
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+let mainVid = document.querySelector(".main-video");
+
+document
+  .querySelectorAll(".courseThree .courseThree__box .courseThree__video video")
+  .forEach((vid) => {
+    vid.onclick = () => {
+      let src = vid.getAttribute("src");
+      mainVid.classList.add("active");
+      mainVid.querySelector("video").src = src;
+    };
+  });
+
+document.querySelector("#close-vid").onclick = () => {
+  mainVid.classList.remove("active");
+};
+
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Education Website Responsive
+
+<hr/>
+
+## Education Website
+
+- Website Education, using HTML, Sass and JavaScript.
+- Includes multipage.
+- Color customization.
+- Developed first with the Mobile First methodology, then for desktop.
+- It contains a Header, Home, About,Contact, Blog, Courses, Teacher and Footer.
+
+### [Live Website](https://lawyer-website-onepage.netlify.app/)
+
+## Used packages cdn
+
+https://boxicons.com/ <br>
+
+<hr/>
+
+### `start`
+
+Open with live server
